3bc74ef04d86c0b01633be59b32ee31dbe2829d4
Surya <suryatk2007@gmail.com>
2025-09-23 18:48:51 +0530

    Feat: Doesn't allow  empty description

--- src/main/java/com/example/demo/Item.java
+++ src/main/java/com/example/demo/Item.java
@@ -4,13 +4,17 @@ package com.example.demo;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Item {
     @Id
     @GeneratedValue
     private Long id;
+
+    @NotBlank
     private String description;
+
     public Item(){
     }
     public Item(String description){
@@ -22,7 +26,10 @@ public class Item {
     public void setId(Long id){
         this.id=id;
     }
-    public String getDescription(String description){
-        return this.description=description;
+    public String getDescription(){
+        return this.description;
+    } 
+    public void setDescription(String description){
+        this.description=description;
     }
 }
